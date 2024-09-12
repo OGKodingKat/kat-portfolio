1742b06206fba443492c24c2b9504d3be96ecc0b
OGKodingKat <167341464+OGKodingKat@users.noreply.github.com>
2024-09-12 12:57:49 -0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -17,7 +17,21 @@ const projectData =[
         description: "In the Ice Cream Ordering Form, I learned HTML form elements, CSS styling, and submitting the form using JavaScript processing. The form incorporates text elements, text area, radio buttons, checkboxes, and a select dropdown.",
         link: "https://ogkodingkat.github.io/ice-cream-form/",
         
-    }
+    },
+    {
+        title: "Betty The Bot",
+        image: "",
+        description: "This bot is designed with preprogrammed responses to common prompts, making it easy to interact with. It also includes a feature where users can submit new prompts via a form, allowing the bot to learn and expand its response set over time. With a focus on simplicity, this bot offers a dynamic and engaging experience while being easy to customize.",
+        link: "https://ogkodingkat.github.io/betty-the-bot/",
+        
+    },
+    {
+        title: "Rock, Paper, Scissors",
+        image: "",
+        description: "This is a simple yet fun rock-paper-scissors game where users can challenge the computer. The player selects their choice—rock, paper, or scissors—while the computer randomly chooses its move. The game then instantly declares a winner based on the classic rules: rock beats scissors, scissors beats paper, and paper beats rock. It’s a quick, easy-to-play game that keeps track of results and delivers endless fun.",
+        link: "https://ogkodingkat.github.io/rock-paper-scissors/",
+        
+    },
 ]
 
 export default projectData;
